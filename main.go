e49f6922922f781e3998d621fa842ba8f4260152
SaintSayang <samir.mebarek@gmail.com>
2023-10-30 16:11:42 +0100

    Adding permissions

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/SamMebarek/orders-api/application"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start()
+	err := app.Start(context.todo)
+	if err != nil {
+		fmt.Println("failed to start app:", err)
 
+	}
 }
