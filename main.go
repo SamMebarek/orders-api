fa2d5fda951b3c6b4da4323bdc4f4d53469f2f7d
SaintSayang <samir.mebarek@gmail.com>
2023-10-30 16:02:21 +0100

    Added proper structure

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/SamMebarek/orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
+	err := app.Start()
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("Failed to listen to server", err)
-	}
-
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
 }
